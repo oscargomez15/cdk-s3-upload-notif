7f4ae8ca35cc9b8aebc30c1d13be76d093d732e7
Oscar G <murdexchannel@gmail.com>
2024-05-30 16:05:20 -0400

    initial commit

--- lib/cdk_project-stack.ts
+++ lib/cdk_project-stack.ts
@@ -1,16 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
+import { SqsDestination } from 'aws-cdk-lib/aws-appconfig';
+import { Bucket, BucketNotificationDestinationType, CfnBucket, EventType } from 'aws-cdk-lib/aws-s3';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import * as s3n from '@aws-cdk/aws-s3-notifications';
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    //L1 and L2 Construct of an S3 Bucket
 
-    // The code that defines your stack goes here
+    const leve1S3Bucket = new CfnBucket(this, 'MyFirstLvl1Construct',{
+      versioningConfiguration:{
+        status: "Enabled"
+      }
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const Level2S3Bucket = new Bucket(this, 'myFirstLvl2ConstructBucket',{
+      bucketName:"s3bucketusingcdk",
+      versioned:true
+    });
+
+    const queue = new Queue(this, "MyQueue",{
+      queueName:"MyQueue"
+    })
+
+    Level2S3Bucket.addEventNotification(EventType.OBJECT_CREATED_COPY, new cdk.aws_s3_notifications.SqsDestination(queue))
   }
 }
